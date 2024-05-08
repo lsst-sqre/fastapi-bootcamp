19a1371e308e0c6eaf0ec550217408b2ac5b948b
Jonathan Sick <jsick@lsst.org>
2024-05-07 20:49:52 -0400

    Fix typo in /error-demo docs

--- src/fastapibootcamp/handlers/external.py
+++ src/fastapibootcamp/handlers/external.py
@@ -380,7 +380,7 @@ class ErrorRequestModel(BaseModel):
     "/error-demo", summary="Raise an internal service exception."
 )
 async def post_error_demo(data: ErrorRequestModel) -> JSONResponse:
-    """Use the custom_error field to compare the different between raising
+    """Use the custom_error field to compare the difference between raising
     a custom SlackException and a generic exception.
     """
     if data.custom_error:
